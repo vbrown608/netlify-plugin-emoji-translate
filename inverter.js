f0163b26c00a038d735c5ef72520061fcb5d7a51
Vivian Brown <vivian@netlify.com>
2020-05-10 22:47:42 -0700

    Ignore two-letter flag words

--- inverter.js
+++ inverter.js
@@ -5,6 +5,8 @@ invert = () => {
   let inverted = {}
   for (let [name, props] of Object.entries(emojilib)) {
     for (const keyword of props.keywords) {
+      if (props.category == "flags" && keyword.length === 2)
+        continue
       if (inverted[keyword]) {
         inverted[keyword].push(props.char)
       } else {
